65675a2a2b1fbf14c87b045cc9932bf30cee5264
Contractor-x <dada4ash@gmail.com>
2026-07-30 14:39:31 +0100

    updated vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        category: resolve(__dirname, 'category.html'),
+        product: resolve(__dirname, 'product.html'),
+        cart: resolve(__dirname, 'cart.html'),
+      },
+    },
   },
 });
