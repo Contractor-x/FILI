5b299ebf003a7a9446b1a560582479beb3e565eb
Contractor-x <dada4ash@gmail.com>
2026-08-01 00:05:20 +0100

    updated home.js

--- js/pages/home.js
+++ js/pages/home.js
@@ -1,3 +1,5 @@
 import { getCartCount } from '../api/cart.js';
+import { renderNavAuthState } from '../components/navAuthState.js';
 
 document.getElementById('cart-count').textContent = getCartCount();
+renderNavAuthState();
