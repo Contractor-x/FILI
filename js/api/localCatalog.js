a3868eb98dd1e6bbf8558e7b8399fb21dc46b910
Contractor-x <dada4ash@gmail.com>
2026-08-03 18:35:49 +0100

    added localcatelog.js

---
+++ js/api/localCatalog.js
@@ -0,0 +1,64 @@
+export const isLocalPreview = location.hostname === 'localhost' || location.hostname === '127.0.0.1';
+
+export const localProducts = [
+  {
+    slug: 'black-up-and-down-set',
+    name: 'Black Up & Down Set',
+    price: 26000,
+    image_url: '/assets/images/products/Black UpandDowns.jpeg',
+  },
+  {
+    slug: 'white-up-and-down-set',
+    name: 'White Up & Down Set',
+    price: 26000,
+    image_url: '/assets/images/products/WhiteUpsandDown.jpeg',
+  },
+  {
+    slug: 'black-hoodie',
+    name: 'Black Hoodie',
+    price: 18500,
+    image_url: '/assets/images/products/Blackhoodie.jpeg',
+  },
+  {
+    slug: 'white-hoodie',
+    name: 'White Hoodie',
+    price: 18500,
+    image_url: '/assets/images/products/Whitehoodie.jpeg',
+  },
+  {
+    slug: 'fili-black-hoodie',
+    name: 'FILI Black Hoodie',
+    price: 19500,
+    image_url: '/assets/images/products/Filihoodieblack.jpeg',
+  },
+  {
+    slug: 'fili-white-hoodie',
+    name: 'FILI White Hoodie',
+    price: 19500,
+    image_url: '/assets/images/products/Filihoodiewhite.jpeg',
+  },
+  {
+    slug: 'black-jacket',
+    name: 'Black Jacket',
+    price: 22000,
+    image_url: '/assets/images/products/Blackjacket.jpeg',
+  },
+  {
+    slug: 'white-jacket',
+    name: 'White Jacket',
+    price: 22000,
+    image_url: '/assets/images/products/Whitejacket.jpeg',
+  },
+  {
+    slug: 'black-jacket-front',
+    name: 'Black Jacket (Front)',
+    price: 22000,
+    image_url: '/assets/images/products/Blackjacketfront.jpeg',
+  },
+  {
+    slug: 'white-jacket-front',
+    name: 'White Jacket (Front)',
+    price: 22000,
+    image_url: '/assets/images/products/Whitejacketfront.jpeg',
+  },
+];
